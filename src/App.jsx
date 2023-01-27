f0e4d747c05dc7930c77c2b0c771ebd531072751
Gustavo Linhaus <gustavo_linhaus@hotmail.com>
2023-01-26 21:03:00 -0300

    Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,15 @@ import { Post } from "./Post";
 export function App() {
   return (
     <div>
-       <Post />
-       <Post />
-       <Post />
+      <Post 
+        author="Gustavo Linhaus" 
+        content="Teste Teste Teste" 
+      />
+
+      <Post 
+        author="Filipe Mattos" 
+        content="Um novo post" 
+      />
     </div>
   )
 }
